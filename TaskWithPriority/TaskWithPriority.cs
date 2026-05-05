2761973c24ad0c0be894b07bdcb86acace6841e4
Gordeeva Tasya <hixtm00@gmail.com>
2026-05-05 22:16:12 +0300

    Hotfix is ready

--- TaskWithPriority/TaskWithPriority.cs
+++ TaskWithPriority/TaskWithPriority.cs
@@ -2,40 +2,23 @@
 
 public enum Priority
 {
-    Низкий,
-    Средний,
-    Высокий
+	Низкий,
+	Средний,
+	Высокий
 }
 
 public class TaskWithPriorityy
 {
-    private string _description;
-    public string Description
-    {
-        get => _description;
-        set
-        {
-            if (value == null)
-                throw new ArgumentNullException(nameof(Description), "Описание не может быть null");
-            if (string.IsNullOrWhiteSpace(value))
-                throw new ArgumentException("Описание не может быть пустым", nameof(Description));
-            _description = value;
-        }
-    }
-    public Priority Priority { get; set; }
-    public bool IsCompleted { get; set; }
-    public DateTime Deadline { get; set; }
+	public string Description { get; set; }
+	public Priority Priority { get; set; }
+	public bool IsCompleted { get; set; }
+	public DateTime Deadline { get; set; }
 
-    public TaskWithPriorityy(string description, Priority priority, DateTime deadline)
-    {
-        if (description == null)
-            throw new ArgumentNullException(nameof(description), "Описание не может быть null");
-        if (string.IsNullOrWhiteSpace(description))
-            throw new ArgumentException("Описание не может быть пустым", nameof(description));
-
-        _description = description;
-        Priority = priority;
-        IsCompleted = false;
-        Deadline = deadline;
-    }
+	public TaskWithPriorityy(string description, Priority priority, DateTime deadline)
+	{
+		Description = description;
+		Priority = priority;
+		IsCompleted = false;
+		Deadline = deadline;
+	}
 }
